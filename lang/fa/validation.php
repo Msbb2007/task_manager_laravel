c45c3e54979ba76a9dcda7b24caaae9e302bb7d0
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-19 20:21:37 +0330

    feat:create lang fix:change lang to fa

---
+++ lang/fa/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'required' => ':attribute حتماً باید پر شود.',
+    'exists' => ':attribute انتخاب شده نامعتبر است.',
+
+    'attributes' => [
+        'name' => 'عنوان',
+        'description' => 'توضیحات',
+        'status' => 'وضعیت',
+        'category_id' => 'دسته‌بندی',
+        'priority' => 'اولویت',
+        'due_date' => 'تاریخ انجام',
+    ],
+];
