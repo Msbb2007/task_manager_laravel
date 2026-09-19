e8ffc889070fce43bd857dff6ef743eabf7ec22b
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-19 18:26:33 +0330

    feat:create new tables

---
+++ database/migrations/2026_09_19_141209_create_tasks_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('status')->default('pending'); // pending or completed
+            $table->string('priority')->default('medium');// low or medium or high
+            $table->dateTime('due_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
