3631fe06c846acb8c6565ba14c1984f6bcfc629c
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-20 21:07:51 +0330

    fix: add name to login's post Route

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
 
 Route::middleware('guest')->group(function () {
     Route::get('/login',[AuthController::class,'showLogin'])->name('login');
-    Route::post('/login',[AuthController::class,'login']);
+    Route::post('/login',[AuthController::class,'login'])->name('login.post');
 
     Route::get('/register',[AuthController::class,'showRegister'])->name('register');
     Route::post('/register',[AuthController::class,'register']);
