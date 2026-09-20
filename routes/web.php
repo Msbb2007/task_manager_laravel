0377f4cf4abad7ce6c05051f47f989c8265d8e2a
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-20 23:58:11 +0330

    fix: add name for register post route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::middleware('guest')->group(function () {
     Route::post('/login',[AuthController::class,'login'])->name('login.post');
 
     Route::get('/register',[AuthController::class,'showRegister'])->name('register');
-    Route::post('/register',[AuthController::class,'register']);
+    Route::post('/register',[AuthController::class,'register'])->name('register.post');;
 });
 
 //admin's panel
