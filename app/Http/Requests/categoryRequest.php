8b03ed0a040ae331019b06a1ff232fe35137cf6f
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-20 13:03:24 +0330

    fix: change authorize to true

--- app/Http/Requests/categoryRequest.php
+++ app/Http/Requests/categoryRequest.php
@@ -12,7 +12,7 @@ class categoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/userRequest.php
+++ app/Http/Requests/userRequest.php
@@ -12,7 +12,7 @@ class userRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
