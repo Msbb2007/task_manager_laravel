95cca77fa518ebd1c434b09586e264f42a9b0e22
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-21 00:03:27 +0330

    fix: add Vazirfont and auth style to head section

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,5 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Task Manager | @yield('title', 'مدیریت وظایف')</title>
+<link href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100..900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('css/auth.css') }}">
 
