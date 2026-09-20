ba7e59f962032e2c331c2ba30e70de31df1b10e0
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-20 13:31:53 +0330

    feat: add new Route to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login',[AuthController::class,'showLogin'])->name('login');
+    Route::post('/login',[AuthController::class,'login']);
+
+    Route::get('/register',[AuthController::class,'showRegister'])->name('register');
+    Route::post('/register',[AuthController::class,'register']);
+});
+
+//admin's panel
+Route::middleware('')->prefix('/admin')->group(function(){
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+//user's panel
+Route::middleware('auth')->prefix('/user')->group(function(){
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 });
