817629f0aab245a715aa9e7b9e1a14b4b2020d97
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-20 21:04:48 +0330

    feat: create master.blade.php in layout and add head section

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    @include('partials.head')
+</head>
+<body>
+<header>
+    <nav>
+        <div class="title">سیستم مدیریت تسک ها</div>
+        <ul>
+            <li><a href="/">خانه</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main class="container">
+    @yield('content')
+</main>
+
+</body>
+</html>
+
---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,5 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Task Manager | @yield('title', 'مدیریت وظایف')</title>
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
