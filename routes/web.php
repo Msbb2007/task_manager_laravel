3385bd97aaedd0d228ae2a0a0a31967eb03aef6b
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-21 14:33:01 +0330

    feat: create admin's panel route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,7 +17,22 @@ Route::middleware('guest')->group(function () {
 });
 
 //admin's panel
-Route::middleware('')->prefix('/admin')->group(function(){
+Route::middleware('auth')->prefix('/admin')->group(function(){
+    Route::get('/dashboard',function (){
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+
+    //users
+    Route::get('/users',[UserController::class,'index'])->name('admin.users');
+    Route::get('/users/{user}',[UserController::class,'show'])->name('admin.users.show');
+
+    //دسترسی فقط برای ادمین
+    Route::middleware(['role:admin'])->group(function (){
+        Route::get('/users/{user}/edit',[UserController::class,'edit'])->name('admin.users.edit');
+        Route::put('/users/{user}',[UserController::class,'update'])->name('admin.users.update');
+        Route::delete('/users/{user}',[UserController::class,'destroy'])->name('admin.users.destroy');
+    });
+
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 });
 
