2538a8634b6faa10769c445fe22c63dfe7f030ca
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-21 19:53:07 +0330

    fix:change description

--- database/migrations/2026_09_19_141209_create_tasks_table.php
+++ database/migrations/2026_09_19_141209_create_tasks_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('category_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->string('title');
-            $table->text('description')->nullable();
-            $table->string('status')->default('pending'); // pending or completed
+            $table->text('description');
+            $table->string('status')->default('pending'); // pending or completed or in_progress
             $table->string('priority')->default('medium');// low or medium or high
             $table->dateTime('due_date')->nullable();
             $table->timestamps();
