9b9d01d721cd6f8c37d53b8d58e42c0cebf2fdac
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-21 14:50:20 +0330

    fix:redirect logout to login's page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -55,5 +55,7 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerate();
+
+        return redirect()->route('login');
     }
 }
