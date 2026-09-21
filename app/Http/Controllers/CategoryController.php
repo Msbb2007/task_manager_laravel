d508bba728efea552c182f844852c021030fe4d1
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-21 20:34:54 +0330

    fix: use Eager Loading =>'withCount()'

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,7 +11,7 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        $categories = Category::query()->paginate(10);
+        $categories = Category::query()->withCount('tasks')->paginate(10);
         return view('admin.categories.index', compact('categories'));
     }
 
