394404ec868c55a0062b0f9240c8abde6f38fca9
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-09-22 20:18:06 +0330

    feat: create new function(showUsers,showTasks_user,deleted_users,hard and soft delete,editProfile) fix: fill the view's address

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\adminEditUserRequest;
+use App\Http\Requests\updateProfileRequest;
 use App\Http\Requests\userRequest;
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -13,13 +16,19 @@ class UserController extends Controller
     public function index()
     {
         $users = User::query()->paginate(10);
-        return view('', compact('users'));
+        return view('admin.users.superAdmin.index', compact('users'));
+    }
+
+    public function showUsers()
+    {
+        $users = User::query()->paginate(10);
+        return view('admin.users.index', compact('users'));
     }
 
 
     public function create()
     {
-        return view('');
+        return view('admin.users.superAdmin.create');
     }
 
     public function store(userRequest $request)
@@ -31,34 +40,82 @@ class UserController extends Controller
             'password' => Hash::make($request->password),
             'role' => $request->role,
         ]);
-        return redirect()->route('')->with('success', 'کاربر با موفقیت ساخته شد');
+        return redirect()->route('admin.users')->with('success', 'کاربر با موفقیت ساخته شد');
     }
 
 
-    public function show(User $user)
+    public function showTasks($userId)
     {
-        return view('', compact('user'));
+        $user = User::query()->findOrFail($userId);
+
+        // گرفتن تمام تسک‌هایی که این کاربر در حال حاضر به آن‌ها وصل است
+        $userTasks = $user->tasks()->with('category')->paginate(5);
+
+        return view('admin.users.tasks', compact('user', 'userTasks'));
     }
 
     public function edit(User $user)
     {
-        return view('', compact('user'));
+        return view('admin.users.superAdmin.edit', compact('user'));
     }
 
-    public function update(userRequest $request, User $user)
+    public function update(adminEditUserRequest $request, User $user)
     {
         $user->update([
             'name' => $request->name,
             'family' => $request->family,
-            'email' => $request->email,
         ]);
-        return redirect()->route('')->with('success', 'اطلاعات کاربر آپدیت شد.');
+        return redirect()->route('admin.users')->with('success', 'اطلاعات کاربر آپدیت شد.');
     }
 
 
-    public function destroy(User $user)
+    public function deleted_users()
     {
+        $deleted_users=User::onlyTrashed()->get();
+        return view('admin.users.superAdmin.deleted_users', compact('deleted_users'));
+    }
+    public function force_delete(string $id)
+    {
+        $user = User::onlyTrashed()->find($id);
+        $user->forceDelete();
+        $deleted_users=User::onlyTrashed()->get();
+        return redirect()->route('admin.users.deleted_users',compact('deleted_users'));
+    }
+
+    public function restore(string $id)
+    {
+        $user = User::onlyTrashed()->find($id);
+        $user->restore();
+
+        $deleted_users=User::onlyTrashed()->get();
+        return redirect()->route('admin.users.deleted_users',compact('deleted_users'));
+    }
+
+    public function soft_delete(string $id)
+    {
+        $user = User::query()->find($id);
         $user->delete();
-        return redirect()->route('')->with('success', 'کاربر با موفقیت حذف شد');
+
+        $deleted_users=User::onlyTrashed()->get();
+        return redirect()->route('admin.users.deleted_users',compact('deleted_users'));
+    }
+
+    public function editProfile(User $user)
+    {
+        return view('admin.users.superAdmin.editProfile', compact('user'));
+    }
+
+    public function updateProfile(string $id, updateProfileRequest $request)
+    {
+        $user = User::query()->findOrFail($id);
+        $data = $request->validate([
+            'email' => 'required|email|unique:users,email,' . $user->id,
+        ]);
+        if ($request->filled('password')) {
+            $data['password'] =  Hash::make($request->password);
+        }
+
+        $user->update($data);
+        return redirect()->route('admin.users')->with('success', 'اطلاعات شما آپدیت شد.');
     }
 }
